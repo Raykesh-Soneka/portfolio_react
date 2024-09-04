cb0df9c5a0c59c567752b525b3d81abec57a56a8
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-09-04 13:28:45 +0200

    On branch MiseEnPlace Your branch is up to date with 'origin/MiseEnPlace'.
    Changes to be committed:
    	new file:   src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+ 
+const App: FunctionComponent = () => {
+    const name: String = 'React';
+
+    return(
+        <h1>Hello, {name}</h1>
+    )
+}
+ 
+export default App;
