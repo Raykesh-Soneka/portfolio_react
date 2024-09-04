4972c1599f2d2e821f184d6829200bd98b001c55
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-09-04 14:14:55 +0200

    On branch MiseEnPlace Your branch is up to date with 'origin/MiseEnPlace'.
    Changes to be committed:
    	modified:   src/App.tsx
    	new file:   src/models/data.ts
    	new file:   src/models/user.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import user from './models/user';
+import data from './models/data' 
 
- 
 const App: FunctionComponent = () => {
-    const name: String = 'React';
+    // const name: String = 'React';
+    const [name, setName] = useState<String>('React');
+    const [users] = useState<user[]>(data);
 
     return(
-        <h1>Bonjour, {name}</h1>
+        <div>
+            <h1>Bonjour, {name}</h1>
+            <p>User : {users.length}</p>
+        </div>
     )
 }
  
